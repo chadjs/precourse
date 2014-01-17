62ab0a85cb354f101657693bf5f90472731cb83d
Chad Sykes <chadsykes@gmail.com>
2014-01-17 11:02:59 -0600

    Completed Exercises

---
+++ Chapter 5/number.rb
@@ -0,0 +1,5 @@
+puts 'What is your favorite number?'
+u_number = gets.chomp.to_i
+n_number = u_number + 1
+puts 'That\'s ok I guess.'
+puts 'But, ' + n_number.to_s + ' would be a bigger, better favorite number!'
